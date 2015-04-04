80b6f1837201711d37456efe7ca810be6f81add2
liulin2012 <liulin.jacob@gmail.com>
2015-04-04 11:39:13 -0400

    --wip--

--- ReverseBits.cpp
+++ ReverseBits.cpp
@@ -17,3 +17,15 @@ public:
         return b2.to_ulong();
     }
 };
+
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        for (int i = 0; i < 32; i++) {
+            res = (res<<1) + (n>>i & 1);
+        }
+        return res;
+    }
+};
